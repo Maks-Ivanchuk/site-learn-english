32166c2ed83f6a849d05a50f1188b1da04da323a
Maks Ivanchuk <maksivanchu@gmail.com>
2023-09-05 09:34:24 +0300

    ghpages-go

---
+++ index.44983732.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const body = document.body;
+    const burgerButton = document.querySelector(".header__burger-menu");
+    const headerNavigation = document.querySelector(".header__nav");
+    burgerButton.addEventListener("click", function(event) {
+        //headerNavigation.classList.toggle('header__nav--open-burger-menu');
+        event.target.closest(".container").querySelector(".header__nav").classList.toggle("header__nav--open-burger-menu");
+        body.classList.toggle("lock");
+    });
+    headerNavigation.addEventListener("click", function(event) {
+        event.target.parentElement.classList.remove("header__nav--open-burger-menu");
+        body.classList.remove("lock");
+    });
+});
+
+//# sourceMappingURL=index.44983732.js.map
