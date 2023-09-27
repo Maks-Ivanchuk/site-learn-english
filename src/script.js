041f9c4e63e788b03bb2ccaba21d96a33a5fc2d6
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-09-27 21:12:26 +0300

    startstart

--- src/script.js
+++ src/script.js
@@ -1,12 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-
    const body = document.body;
    const burgerButton = document.querySelector('.header__burger-menu');
    const headerNavigation = document.querySelector('.header__nav');
 
    burgerButton.addEventListener('click', function (event) {
-      //headerNavigation.classList.toggle('header__nav--open-burger-menu');
       event.target.closest('.container').querySelector('.header__nav').classList.toggle('header__nav--open-burger-menu');
       body.classList.toggle('lock');
    });
